72f2376e9b066766956a28cb82dd03b5a08ed4f2
fares waheed <faressalm44@hotmail.com>
2022-11-29 23:23:56 +0200

    extract token  names

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ class LexicalAnalyzerGenerator{
 private:
     string lexicalRulesPath;
     unordered_map<string,NFA*> RegularDefs;
+    unordered_set<char> fixedRulesChars = {'-','|','+','(',')','*'};
 public:
     LexicalAnalyzerGenerator(string lexicalRulesPath): lexicalRulesPath(lexicalRulesPath){}
     vector<NFA> generateNFAs(){
@@ -32,14 +33,35 @@ public:
         string lexicalRuleLine;
         if (file.is_open()){
             while (getline(file, lexicalRuleLine))
-            {
-
+            {   if(lexicalRuleLine[0]!='{' && lexicalRuleLine[0]!='[')
+                    getNFAFromRE(lexicalRuleLine);
             }
             file.close();
         }
         return {};
     }
-
+private:
+    //NFA
+    void getNFAFromRE(string lexicalRuleLine){
+        pair<string,int> tokenNameAndIndex = getTokenName(lexicalRuleLine);
+        bool isRE = (lexicalRuleLine[tokenNameAndIndex.second] == '=');
+        for (int i = tokenNameAndIndex.second + 1; i < lexicalRuleLine.size() ; ++i) {
+            while (lexicalRuleLine[i]==' ') i++;
+        }
+        cout<<tokenNameAndIndex.first<<endl;
+    }
+    // first -> token name, second-> index of assign operator for RE or RD
+    pair<string,int> getTokenName(string lexicalRuleLine){
+        int i = 0;
+        while (lexicalRuleLine[i]==' ') i++;
+        string tokenName="";
+        while (lexicalRuleLine[i]!='='&&lexicalRuleLine[i]!=':') {
+            if(lexicalRuleLine[i]!=' ')
+                tokenName+=lexicalRuleLine[i];
+            i++;
+        }
+        return {tokenName,i};
+    }
 };
 
 int main() {
