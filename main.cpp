a69de648b6ab3f1bbe856f74d63107caa4280ff8
Ali <aliahmedfahmy2@gmail.com>
2022-12-03 11:06:25 +0200

    Adding NAF implementation in NAF branch.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+/**
 class State{
     public:
         int index;
@@ -41,10 +42,53 @@ public:
     }
 
 };
+**/
+#include "NFA/NFA.h"
 
 int main() {
-    LexicalAnalyzerGenerator lexicalAnalyzerGenerator =  LexicalAnalyzerGenerator("..\\lexical_rules.txt");
-    lexicalAnalyzerGenerator.generateNFAs();
+    //LexicalAnalyzerGenerator lexicalAnalyzerGenerator =  LexicalAnalyzerGenerator("..\\lexical_rules.txt");
+    //lexicalAnalyzerGenerator.generateNFAs();
+    vector<queue<pair<string,bool>>> REs ;
+    vector<pair<string, int>> tokens;
+    queue<pair<string,bool>> re ;
+    queue<pair<string,bool>> re2 ;
+    re.push({"i",false});
+    re.push({"n",false});
+    re.push({".",true});
+    re.push({"t",false});
+    re.push({".",true});
+    //tokens.push_back({"int", 0});
+    //REs.push_back(re);
+
+    re2.push({"a",false});
+    //re2.push({"z",false});
+    re2.push({"*",true});
+    /**
+    re2.push({"A",false});
+    re2.push({"Z",false});
+    re2.push({"-",true});
+    re2.push({"|",true});
+
+    re2.push({"a",false});
+    re2.push({"z",false});
+    re2.push({"-",true});
+    re2.push({"A",false});
+    re2.push({"Z",false});
+    re2.push({"-",true});
+    re2.push({"|",true});
+    re2.push({"0",false});
+    re2.push({"9",false});
+    re2.push({"-",true});
+    re2.push({"|",true});
+    re2.push({"*",true});
+    re2.push({".",true});
+    **/
+    tokens.push_back({"id", 1});
+    REs.push_back(re2);
+    NFA_builder builder;
+    NFA c = builder.build(REs, tokens);
+
+
 
     return 0;
 }
